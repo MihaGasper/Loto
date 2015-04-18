40efe3f0b9668bfe03eab994d38878a033559b62
MihaGasper <miha.gaspersic@gmail.com>
2015-04-18 21:56:07 +0200

    Revert "prenos2"
    This reverts commit dfbc1bf1e4c4049efdec2ab1293068e27cd48680.

--- loto.py
+++ loto.py
@@ -1,11 +1,9 @@
 import random
-list =[]
 n = int(raw_input("vpisi koliko random stevil zelis"))
 
 def loto(a,b):
     for i in range(n):
-        list.append(int(random.randint(a,b)))
-    return (list)
+        return int(random.randint(a,b))
 
 
 print loto(int(raw_input(("min.mozna izrebana stevilka"))),int(raw_input("max.mozna izrebana stevilka")))
