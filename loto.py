96868588930ca834deef11740653baff5b7b9004
MihaGasper <miha.gaspersic@gmail.com>
2015-04-18 22:26:49 +0200

    p3

--- loto.py
+++ loto.py
@@ -1,15 +1,13 @@
 import random
+list =[]
 n = int(raw_input("vpisi koliko random stevil zelis"))
 
 def loto(a,b):
     for i in range(n):
-        return int(random.randint(a,b))
+        list.append(int(random.randint(a,b)))
+    return (list)
 
 
 print loto(int(raw_input(("min.mozna izrebana stevilka"))),int(raw_input("max.mozna izrebana stevilka")))
-
-
-
-
 
 
